5af0841d4a724a635e2e8c3571643f1282fd47bf
LUCAS DA SILVEIRA <lucas_silveira11@estudante.sesisenai.org.br>
2023-03-13 20:10:37 -0300

    Adicionado 'lower' às queries + alteração de nome de rota e função

--- routes/cerveja.js
+++ routes/cerveja.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/cerveja");
 
 router.get('/nome/:nome', controller.getNome);
+router.get('/nome/abreviado/:nome', controller.getNomeAbr);
 router.get('/nacionalidade/:nacionalidade', controller.getNac);
 router.get('/tipo/:tipo', controller.getTipo);
 router.get('/abv', controller.getAbv);
-router.get('/consulta/:nomeP', controller.getNomeP);
 
 module.exports = router;
